4e6539c4e735ff04c0324d698818d263b4d2ca5e
Wesley Godoi <wesleygodoi@gmail.com>
2021-07-07 21:57:01 -0300

    Criada função para remoção de pergunta.

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
@@ -7,6 +8,7 @@ import { useRoom } from '../hooks/useRoom';
 
 
 import '../styles/room.scss';
+import { database } from '../services/firebase';
 
 type RoomParams = {
   id: string;
@@ -17,6 +19,12 @@ export function AdminRoom() {
   const roomId = params.id;
   
   const { title, questions} = useRoom(roomId)
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')){
+       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
   
   return (
     <div id="page-room">
@@ -42,7 +50,14 @@ export function AdminRoom() {
               key={question.id}
               content={question.content}
               author={question.author}
-             />
+             >
+               <button
+                 type="button"
+                 onClick={()=> handleDeleteQuestion(question.id)}
+               >
+                <img src={deleteImg} alt="Remover Pergunta" />  
+               </button>
+             </Question>  
             )
          })}
         </div>
