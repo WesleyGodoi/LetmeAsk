a85cb70a7f1207341e1606a97f4d11a9b6a1efff
Wesley Godoi <wesleygodoi@gmail.com>
2021-07-08 21:14:44 -0300

    Adicionado componente classnames.

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -71,23 +71,27 @@ export function AdminRoom() {
            return (
              <Question
               key={question.id}
-              content={question.content}
+              content={question.content} 
               author={question.author}
+              isAnswered={question.isAnswered}
+              isHighlighted={question.isHighlighted}
              >
-               <button
-                 type="button"
-                 onClick={()=> handleCheckQuestionAsAnswered(question.id)}
-               >
-                <img src={checkImg} alt="Marcar pergunta como respondida" />  
-               </button>
-
-               <button
-                 type="button"
-                 onClick={()=> handleHighlightQuestion(question.id)}
-               >
-                <img src={answerImg} alt="Dar destaque à pergunta" />  
-               </button>
-
+              {!question.isAnswered && (
+                <>
+                     <button
+                      type="button"
+                      onClick={()=> handleCheckQuestionAsAnswered(question.id)}
+                     >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />  
+                   </button>
+                   <button
+                     type="button"
+                     onClick={()=> handleHighlightQuestion(question.id)}
+                   >
+                     <img src={answerImg} alt="Dar destaque à pergunta" />  
+                   </button>
+                </>
+              )}
                <button
                  type="button"
                  onClick={()=> handleDeleteQuestion(question.id)}
